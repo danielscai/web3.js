75a1108b37152a1cedffc8d7814754f8b7feb49a
yunfei.cai <yunfei.cai@datayes.com>
2017-08-30 08:42:36 +0000

    use test contract

--- dcai-example/node-app.js
+++ dcai-example/node-app.js
@@ -17,3 +17,20 @@ var new_balance = balance.plus(21).toString(10);
 console.log(new_balance);
 console.log(web3.version.api);
 console.log(web3.version.network);
+console.log(web3.version.ethereum);
+console.log(web3.eth.mining);
+//console.log(web3.eth.fromWei(web3.eth.gasPrice.toString(10)));
+
+
+web3.eth.getAccounts(function (error, result){
+    console.log(result)
+}
+)
+
+var abi = [{"constant":true,"inputs":[{"name":"a","type":"uint256"}],"name":"multiply","outputs":[{"name":"d","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"}]
+var c = web3.eth.contract(abi);
+var contract = c.at('0xb1e23ce2dd190d30e7c0d4f5907d43814d8c96e8');
+
+result = contract.multiply(7)
+console.log("print result")
+console.log(result)
