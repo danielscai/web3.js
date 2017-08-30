398c0ce367f34e961f242768e7829210ddcbcec5
daniels_cai <daniels_cai@zeuscloud.cn>
2017-08-30 08:52:16 +0800

    balane plus

---
+++ dcai-example/node-app.js
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+
+var Web3 = require('../index.js');
+var web3 = new Web3();
+
+web3.setProvider(new web3.providers.HttpProvider('http://localhost:8545'));
+
+var coinbase = web3.eth.coinbase;
+console.log(coinbase);
+
+var balance = web3.eth.getBalance(coinbase);
+console.log(balance.toString(10));
+
+//var balance = new BigNumber('131242344353464564564574574567456');
+console.log('add 21 to balance');
+var new_balance = balance.plus(21).toString(10);
+console.log(new_balance);
