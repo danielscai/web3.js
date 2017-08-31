b7be9495f07633199ec98bf223b3ffc150bfe955
yunfei.cai <yunfei.cai@datayes.com>
2017-08-31 12:51:50 +0000

    use new contract

--- dcai-example/get_status.js
+++ dcai-example/get_status.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 
+//var contract_address = '0x293fec0955c00e31eed812f61dbe65f93eb3616b';
 var contract_address = '0x5c7ab21c67de17d43531a6740f581898acede017';
 var Web3 = require('../index.js');
 var web3 = new Web3();
@@ -12,7 +13,7 @@ console.log(web3.fromWei(web3.eth.getBalance(contract_address).toString(10)));
 web3.eth.getBalance(contract_address, function(error, result){
     console.log('contract balance');
     console.log(error);
-    console.log(result);
+    console.log(web3.fromWei(result.toString(10)));
 });
 
 var abi = [{"constant":false,"inputs":[],"name":"bet","outputs":[],"payable":true,"stateMutability":"payable","type":"function"},{"constant":false,"inputs":[{"name":"random_index","type":"uint256"}],"name":"__callback","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[],"name":"withdraw","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"new_bet_count","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"b_c","type":"uint256"}],"name":"setbetcount","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[],"name":"close","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"minimal_bet","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"bet_status","outputs":[{"name":"","type":"uint8"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"m_b","type":"uint256"},{"name":"b_c","type":"uint256"}],"name":"setminiandcount","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"random_index","type":"uint256"}],"name":"openlottery","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"index","type":"uint256"}],"name":"getBalancesKV","outputs":[{"name":"key","type":"address"},{"name":"value","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"current_bet","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"total_bet","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"acceptOwnership","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"balances","outputs":[{"name":"size","type":"uint256"},{"name":"num","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"bet_count","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"owner","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"m_b","type":"uint256"}],"name":"setminimalbet","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"max_percentage","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_newOwner","type":"address"}],"name":"changeOwner","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"newOwner","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"new_minimal_bet","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"inputs":[{"name":"mini","type":"uint256"},{"name":"b_c","type":"uint256"},{"name":"m_p","type":"uint256"}],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"payable":true,"stateMutability":"payable","type":"fallback"},{"anonymous":false,"inputs":[{"indexed":true,"name":"sender","type":"address"},{"indexed":true,"name":"value","type":"uint256"}],"name":"Beted","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"sender","type":"address"},{"indexed":true,"name":"value","type":"uint256"}],"name":"Withdraw","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"lottery_reveiver","type":"address"},{"indexed":true,"name":"lottery_value","type":"uint256"}],"name":"LotteryHit","type":"event"}]
@@ -20,9 +21,7 @@ var abi = [{"constant":false,"inputs":[],"name":"bet","outputs":[],"payable":tru
 var c = web3.eth.contract(abi);
 var contract = c.at(contract_address);
 
-contract.bet({from: coinbase, value: web3.toWei('2','ether') , gas: 200000}, function (error, result){
-    console.log('print bet result');
-    console.log(error);
-    console.log(result);
-});
+var state = web3.eth.getStorageAt(contract_address, 11);
+console.log('get storage');
+console.log(state);
 
